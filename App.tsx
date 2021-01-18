82c1a737476fd2a1958be71d2086114a7a41629f
Shahrukh98 <shahrukhnawaz98@gmail.com>
2021-01-18 21:50:23 +0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import Chat from "./src/screens/Chat";
+import Splash from "./src/screens/Splash";
+
+const App = () => {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    const load = setTimeout(() => {
+      setInit(true);
+    }, 2000);
+    return () => {
+      clearTimeout(load);
+    };
+  }, []);
+
+  if (init) {
+    return <Chat />;
+  } else {
+    return <Splash />;
+  }
+};
+
+export default App;
---
+++ src/screens/Chat/index.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { GiftedChat, IMessage } from "react-native-gifted-chat";
+
+const Chat = () => {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  );
+};
+
+export default Chat;
